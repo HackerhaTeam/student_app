d50bf03c6f2c2004029bec23e7286fc36ca52931
belalakkad <belalakkad229@gmail.com>
2025-04-20 11:50:34 +0300

    create quiz_timer.dart file

--- lib/features/quiz/presentation/pages/widgets/quiz_page_body.dart
+++ lib/features/quiz/presentation/pages/widgets/quiz_page_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:student_hackerha/core/functions/get_responsive_size.dart';
+import 'package:student_hackerha/core/themes/extentions/app_backgrounds.dart';
+import 'package:student_hackerha/core/themes/typoGraphy/app_text_styles.dart';
 import 'package:student_hackerha/features/quiz/presentation/pages/widgets/quiz_page_header.dart';
 
 class QuizPageBody extends StatelessWidget {
@@ -7,6 +10,8 @@ class QuizPageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final backgrounds = Theme.of(context).extension<AppBackgrounds>()!;
+
     return Padding(
       padding: EdgeInsets.symmetric(
           horizontal: 20.w(context), vertical: 10.h(context)),
@@ -17,14 +22,33 @@ class QuizPageBody extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                '10 / 1',
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-
-              Text(
-                '30:00',
-                style: Theme.of(context).textTheme.labelMedium,
+              Text('1 / 10', style: Theme.of(context).textTheme.xLabelLarge),
+              Container(
+                decoration: BoxDecoration(
+   color: backgrounds.onSurfaceSecondary,
+   borderRadius: BorderRadius.circular(12.r(context))
+                ),
+                width: 86.w(context),
+                height: 30.h(context),
+             
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PhosphorIcon(
+                        PhosphorIcons.timer(),
+                        size: 16.w(context),
+                      ),
+                      SizedBox(
+                        width: 4.w(context),
+                      ),
+                      Text(
+                        '30:00',
+                        style: Theme.of(context).textTheme.xLabelMedium,
+                      ),
+                    ],
+                  ),
+                ),
               )
             ],
           )
