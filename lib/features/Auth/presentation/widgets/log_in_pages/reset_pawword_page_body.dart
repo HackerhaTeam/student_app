5b1b099f3ae64656a00dcec77ddb4d434007f2f4
AhmadAlsakani <alsakniahmad51@gmail.com>
2025-04-24 18:21:08 +0300

    refactor code

--- lib/features/Auth/presentation/widgets/log_in_pages/reset_pawword_page_body.dart
+++ lib/features/Auth/presentation/widgets/log_in_pages/reset_pawword_page_body.dart
@@ -2,16 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'package:student_hackerha/core/constants/assets.dart';
 import 'package:student_hackerha/core/functions/get_responsive_size.dart';
 import 'package:student_hackerha/core/themes/typoGraphy/app_text_styles.dart';
-import 'package:student_hackerha/core/util/navigator.dart';
-import 'package:student_hackerha/core/widgets/custom_success_dialog.dart';
 import 'package:student_hackerha/core/widgets/custom_text_field.dart';
 import 'package:student_hackerha/core/widgets/float_next_button_with_dialog.dart';
 import 'package:student_hackerha/features/Auth/presentation/pages/sign_up_pages/sign_up_wrapper.dart';
 import 'package:student_hackerha/features/Auth/presentation/widgets/buttons/back_button.dart';
-import 'package:student_hackerha/features/Auth/presentation/widgets/buttons/floating_next_button.dart';
 import 'package:student_hackerha/core/widgets/headers/introduction_header.dart';
 
 class ResetPasswordPageBody extends StatefulWidget {
