7991d51dcb7816782e0ae9f36634a96e09678ba1
Sami barsa <sami5114943@gmai.com>
2025-08-28 00:34:49 +0300

    add fade animation to noCourseFound widget

--- lib/features/courses/presentation/widgets/no_courses_foundbody.dart
+++ lib/features/courses/presentation/widgets/no_courses_foundbody.dart
@@ -5,6 +5,7 @@ import 'package:student_hackerha/core/functions/get_responsive_size.dart';
 import 'package:student_hackerha/core/helpers/tags/get_theme_icon.dart';
 import 'package:student_hackerha/core/themes/extentions/app_content.dart';
 import 'package:student_hackerha/core/themes/typoGraphy/app_text_styles.dart';
+import 'package:student_hackerha/core/widgets/animation/fade_widget.dart';
 import 'package:student_hackerha/features/courses/presentation/widgets/courses_headdr.dart';
 
 class NoCoursesFoundbody extends StatelessWidget {
@@ -16,31 +17,33 @@ class NoCoursesFoundbody extends StatelessWidget {
   Widget build(BuildContext context) {
     final content = Theme.of(context).extension<AppContent>();
 
-    return Column(
-      children: [
-     
-        SizedBox(height: 80.h(context),),
-        SvgPicture.asset(
-          getThemeIcon(context, AppImages.noCoursesImageDark, AppImages.noCoursesImageLight)),
-        Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            Text(
-              "لا دورات متاحة الآن",
-              style: context.xDisplayLarge,
-            ),
-            SizedBox(
-              width: double.infinity,
-            ),
-            Text(
-              "ترقبوا! سيتم إضافة دورات جديدة قريبًا.",
-              style: context.xParagraphLargeNormal
-                  .copyWith(color: content!.tertiary),
-            ),
-            Spacer()
-          ],
-        )
-      ],
+    return FadeInWidget(
+      child: Column(
+        children: [
+       
+          SizedBox(height: 80.h(context),),
+          SvgPicture.asset(
+            getThemeIcon(context, AppImages.noCoursesImageDark, AppImages.noCoursesImageLight)),
+          Wrap(
+            alignment: WrapAlignment.center,
+            children: [
+              Text(
+                "لا دورات متاحة الآن",
+                style: context.xDisplayLarge,
+              ),
+              SizedBox(
+                width: double.infinity,
+              ),
+              Text(
+                "ترقبوا! سيتم إضافة دورات جديدة قريبًا.",
+                style: context.xParagraphLargeNormal
+                    .copyWith(color: content!.tertiary),
+              ),
+              Spacer()
+            ],
+          )
+        ],
+      ),
     );
   }
 }
